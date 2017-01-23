dfc917487aca3dad9930a50cf9fbf47cc3f0dbc3
v.litvinenko <cleontinka@gmail.com>
2017-01-23 15:07:43 +0200

    Почистила файл registration от лишних символов

--- resources/views/pages/registration.blade.php
+++ resources/views/pages/registration.blade.php
@@ -1,6 +1,5 @@
 <div class="registration-bg">
     <div class="container">
-
         <form class="form-signin wow fadeInUp" method="POST" enctype="application/x-www-form-urlencoded" action="{{ route('site.auth.registrationPost') }}">
             {{ csrf_field() }}
             <h2 class="form-signin-heading">Register now</h2>
@@ -10,7 +9,6 @@
                 <?php echo $errors->first('name'); ?>
                 <input type="text" name="email" class="form-control" placeholder="Email" autofocus="">
                 <?php echo $errors->first('email'); ?>
-
                 <p> Enter account details below</p>
                 <input type="text" name="phone" class="form-control" placeholder="8 (999) 123-45-67" autofocus="">
                 <?php echo $errors->first('phone'); ?>
@@ -23,7 +21,6 @@
                     <?php echo $errors->first('is_confirmed'); ?>
                 </label>
                 <button class="btn btn-lg btn-login btn-block" type="submit">Submit</button>
-
                 <div class="registration">
                     Already Registered ?
                     <a class="" href="login.html">
